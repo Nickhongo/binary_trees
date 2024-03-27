6121dcb3b5b06ac7bbbf11e63ac69211be28231e
Nickhongo <hongonicholas0@gmail.com>
2024-03-27 09:24:15 +0300

    Write a function that goes through a binary tree using in-order traversal

--- 6-binary_tree_preorder.c
+++ 6-binary_tree_preorder.c
@@ -16,3 +16,18 @@ void binary_tree_preorder(const binary_tree_t *tree, void (*func)(int))
 	binary_tree_preorder(tree->left, func);
 	binary_tree_preorder(tree->right, func);
 }
+#include "binary_trees.h"
+/**
+ * binary_tree_is_leaf - checks if node is a leaf
+ * @node: pointer to the node to check
+ *
+ * Return: 1 or 0
+ */
+
+int binary_tree_is_leaf(const binary_tree_t *node)
+{
+	if (node == NULL)
+		return (0);
+
+	return ((node->left == NULL && node->right == NULL) ? 1 : 0);
+}
